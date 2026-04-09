00455d84f4c17362bce0e552c1c7e6171b18065d
Denys Ovcharov <denisovcharov88@gmail.com>
2026-04-09 22:59:20 +0200

    hw-5.2

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -4,10 +4,14 @@ import {
   logoutUser,
   refreshUserSession,
   registerUser,
+  requestResetEmail,
+  resetPassword,
 } from '../controllers/authController.js';
 import {
   loginUserValidation,
   registerUserValidation,
+  requestResetEmailSchema,
+  resetPasswordSchema,
 } from '../validations/authValidation.js';
 
 const router = Router();
@@ -16,5 +20,11 @@ router.post('/auth/register', registerUserValidation, registerUser);
 router.post('/auth/login', loginUserValidation, loginUser);
 router.post('/auth/logout', logoutUser);
 router.post('/auth/refresh', refreshUserSession);
+router.post(
+  '/auth/request-reset-email',
+  requestResetEmailSchema,
+  requestResetEmail,
+);
+router.post('/auth/reset-password', resetPasswordSchema, resetPassword);
 
 export default router;
--- src/validations/authValidation.js
+++ src/validations/authValidation.js
@@ -13,3 +13,16 @@ export const loginUserValidation = celebrate({
     password: Joi.string().required(),
   }),
 });
+
+export const requestResetEmailSchema = celebrate({
+  [Segments.BODY]: Joi.object({
+    email: Joi.string().email().required(),
+  }),
+});
+
+export const resetPasswordSchema = celebrate({
+  [Segments.BODY]: Joi.object({
+    password: Joi.string().min(8).required(),
+    token: Joi.string().required(),
+  }),
+});
