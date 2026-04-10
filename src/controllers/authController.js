318e59947324c92b6888565e5207cc44b6ea4762
Denys Ovcharov <denisovcharov88@gmail.com>
2026-04-10 20:54:07 +0200

    hw-5.8

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -8,7 +8,7 @@ import {
 } from '../services/auth.js';
 import { Session } from '../models/session.js';
 import jwt from 'jsonwebtoken';
-import { sendEmail } from '../utils/sendMail.js';
+import { sendMail } from '../utils/sendMail.js';
 import handlebars from 'handlebars';
 import path from 'node:path';
 import fs from 'node:fs/promises';
@@ -146,7 +146,7 @@ export const requestResetEmail = async (req, res) => {
   });
 
   try {
-    await sendEmail({
+    await sendMail({
       from: process.env.SMTP_FROM,
       to: email,
       subject: 'Reset your password',
