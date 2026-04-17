05158c3f3acde002ab3c0ae8826b9d6aeec68d27
Denys Ovcharov <denisovcharov88@gmail.com>
2026-04-17 11:18:42 +0200

    updated

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -5,14 +5,13 @@ export const getAllNotes = async (req, res) => {
   const { page = 1, perPage = 10, tag, search, isFavourite } = req.query;
 
   const skip = (page - 1) * perPage;
-
-  const notesQuery = Note.find({ userId: req.user._id, isTrashed: false });
-
   const filter = { userId: req.user._id, isTrashed: false };
 
   if (isFavourite === 'true') {
     filter.isFavourite = true;
   }
+
+  const notesQuery = Note.find(filter);
 
   if (search) {
     notesQuery.where({ $text: { $search: search } });
